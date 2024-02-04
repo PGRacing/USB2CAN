4276a548dd99216b317099a19d84f6046e618146
OliwerL <oliwer.lisek@wp.pl>
2024-02-04 16:52:03 +0100

    second attempt

---
+++ CODE/PC_APP/CanApp/ViewModels/SecondWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace WpfApp1.ViewModels
+{
+    public class SecondWindowViewModel : INotifyPropertyChanged
+    {
+        public MyViewModel MainViewModel { get; private set; }
+
+        private int _selectedId;
+        public int SelectedId
+        {
+            get { return _selectedId; }
+            set
+            {
+                if (_selectedId != value)
+                {
+                    _selectedId = value;
+                    OnPropertyChanged(nameof(SelectedId));
+                    FilterDataGridCollection();
+                }
+            }
+        }
+
+        private ObservableCollection<MyDataModel> _filteredDataGridCollection;
+        public ObservableCollection<MyDataModel> FilteredDataGridCollection
+        {
+            get { return _filteredDataGridCollection; }
+            set
+            {
+                if (_filteredDataGridCollection != value)
+                {
+                    _filteredDataGridCollection = value;
+                    OnPropertyChanged(nameof(FilteredDataGridCollection));
+                }
+            }
+        }
+
+        public SecondWindowViewModel(MyViewModel mainViewModel)
+        {
+            MainViewModel = mainViewModel;
+            FilteredDataGridCollection = new ObservableCollection<MyDataModel>();
+        }
+
+        private void FilterDataGridCollection()
+        {
+            if (MainViewModel != null && MainViewModel.DataGridCollection != null)
+            {
+                // Pobierz przefiltrowane dane z DataGridCollection w oparciu o wybrane ID
+                var filteredData = MainViewModel.DataGridCollection.Where(item => item.ID == SelectedId);
+
+                // Zaktualizuj przefiltrowaną kolekcję
+                FilteredDataGridCollection.Clear();
+                foreach (var item in filteredData)
+                {
+                    FilteredDataGridCollection.Add(item);
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
